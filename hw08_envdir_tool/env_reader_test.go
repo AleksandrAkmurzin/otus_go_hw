088445d81144b8920c7439ab6b126e7015298164
Akmurzin Aleksandr <Aleksandr.Akmurzin@x5.ru>
2022-05-28 19:17:42 +0300

    HW08 Implement env reader

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,43 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("Wrong dir name", func(t *testing.T) {
+		_, err := ReadDir("testdata")
+		if err != nil {
+			fmt.Println(err)
+		}
+		require.Error(t, err)
+	})
+
+	t.Run("Wrong file name", func(t *testing.T) {
+		info, err := os.Stat("testdata/wrong=name")
+		require.NoError(t, err)
+
+		_, err = fileToEnvValue("testdata", info)
+		require.ErrorIs(t, err, ErrUnsupportedFileName)
+	})
+
+	t.Run("Positive case", func(t *testing.T) {
+		actualEnv, err := ReadDir("testdata/env")
+		require.NoError(t, err)
+
+		expectedEnv := Environment{
+			"BAR":   EnvValue{Value: "bar"},
+			"EMPTY": EnvValue{},
+			"FOO":   EnvValue{Value: "   foo\nwith new line"},
+			"HELLO": EnvValue{Value: "\"hello\""},
+			"UNSET": EnvValue{NeedRemove: true},
+		}
+
+		for name, value := range expectedEnv {
+			require.Equal(t, value, actualEnv[name], "Value for %s env param is wrong", name)
+		}
+	})
 }
