d32081fc8f6411b08aaa7947b972b92127350f66
Akmurzin Aleksandr <Aleksandr.Akmurzin@x5.ru>
2022-07-03 15:17:44 +0300

    HW08 Implement cmd executor

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,47 @@
 package main
 
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
+	command := exec.Command(cmd[0], cmd[1:]...)
+
+	for envName, envValue := range env {
+		if err := processEnv(envName, envValue); err != nil {
+			log.Fatal(err)
+		}
+	}
+	command.Env = os.Environ()
+
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	if err := command.Run(); err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			return exitError.ExitCode()
+		}
+	}
+
+	return
+}
+
+func processEnv(envName string, value EnvValue) (err error) {
+	_, ok := os.LookupEnv(envName)
+
+	if !value.NeedRemove {
+		err = os.Setenv(envName, value.Value)
+		return
+	}
+
+	if !ok {
+		return
+	}
+
+	err = os.Unsetenv(envName)
 	return
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,84 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	environment, err := ReadDir("testdata/env")
+	require.NoError(t, err)
+
+	t.Run("OK", func(t *testing.T) {
+		returnCode := RunCmd([]string{"testdata/echo.sh", "arg"}, environment)
+		require.Equal(t, 0, returnCode)
+	})
+
+	t.Run("ERR", func(t *testing.T) {
+		returnCode := RunCmd([]string{"testdata/error.sh"}, environment)
+		require.Equal(t, 1, returnCode)
+	})
+}
+
+func TestProcessEnv(t *testing.T) {
+	testEnvName := "testEnvName"
+	testEnvValue := "testEnvValue"
+
+	t.Run("Add", func(t *testing.T) {
+		_, exist := os.LookupEnv(testEnvName)
+		require.False(t, exist)
+
+		err := processEnv(testEnvName, EnvValue{testEnvValue, false})
+		require.NoError(t, err)
+
+		addedValue, ok := os.LookupEnv(testEnvName)
+		require.True(t, ok)
+		require.Equal(t, testEnvValue, addedValue)
+	})
+
+	t.Run("Replace", func(t *testing.T) {
+		_, exist := os.LookupEnv(testEnvName)
+		if !exist {
+			err := os.Setenv(testEnvName, testEnvValue)
+			require.NoError(t, err)
+		}
+
+		err := processEnv(testEnvName, EnvValue{testEnvValue, false})
+		require.NoError(t, err)
+
+		replacedValue, ok := os.LookupEnv(testEnvName)
+		require.True(t, ok)
+		require.Equal(t, testEnvValue, replacedValue)
+	})
+
+	t.Run("Remove existed", func(t *testing.T) {
+		_, exist := os.LookupEnv(testEnvName)
+		if !exist {
+			err := os.Setenv(testEnvName, testEnvValue)
+			require.NoError(t, err)
+		}
+
+		err := processEnv(testEnvName, EnvValue{testEnvValue, true})
+		require.NoError(t, err)
+
+		removedValue, ok := os.LookupEnv(testEnvName)
+		require.False(t, ok)
+		require.Empty(t, removedValue)
+	})
+
+	t.Run("Remove not existed", func(t *testing.T) {
+		_, exist := os.LookupEnv(testEnvName)
+		if exist {
+			err := os.Unsetenv(testEnvName)
+			require.NoError(t, err)
+		}
+
+		err := processEnv(testEnvName, EnvValue{testEnvValue, true})
+		require.NoError(t, err)
+
+		removedValue, ok := os.LookupEnv(testEnvName)
+		require.False(t, ok)
+		require.Empty(t, removedValue)
+	})
 }
